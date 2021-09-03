33a6ff627af1eabbdd3239069023950e234b56a7
JonatanESalinas <A01731815@itesm.mx>
2021-09-03 18:21:43 -0500

    First commit

---
+++ act_1p6_paralelo.c
@@ -0,0 +1,114 @@
+/*
+    Actividad 1.6 Multiprocesadores
+    Programa con paralelismo
+
+    Jonatan Emanuel Salinas Avila       A01731815
+    3/Sept/2021
+*/
+
+#include<stdio.h>
+#include <math.h>
+#include "omp.h"
+
+#define PI 3.14159265
+
+static long mi_N = 50000;
+
+double function1(double ti, double wi){
+    return ( (ti*exp(3*ti)) - (2*wi) );
+}
+
+double function2(double ti, double wi){
+    return ( 1 + pow(ti-wi,2) );
+}
+
+double function3(double ti, double wi){
+    return ( 1 + (wi/ti) );
+}
+
+double function4(double ti, double wi){
+    return ( cos(2*ti*wi) + sin(3*ti*wi) );
+}
+
+void rungekuta(int valor_n, char * nombre_file, int que_ecuacion){
+        
+    FILE * fptr;
+    
+    long N = valor_n;
+    fptr=fopen(nombre_file,"w");
+
+    fprintf(fptr, "Datos que encuentra el metodo de Rungecuta(variable ind.\t variable dep.\t numero de thread)\n");
+    double h,t,w;
+    double w0=PI/4,a=0,b=PI;
+    int i;
+    w=w0;
+
+    fprintf(fptr, "%f\t %f\n", a, w);
+    for(i=0;i<N;i++){
+        h=(b-a)/N;
+        t=a+(h*i);
+
+        if(que_ecuacion == 1){
+            double k1 = h*(function1(t, w));
+            double k2 = h*(function1(t + (h/2.0), w + (k1/2.0)) );
+            double k3 = h*(function1(t + (h/2.0), w + (k2/2.0)) ); 
+            double k4 = h*(function1(t + h, w + k3));
+            w = w + (1.0/6.0)*(k1 + 2*k2 + 2*k3 + k4);
+        }
+        else if(que_ecuacion == 2){
+            double k1 = h*(function2(t, w));
+            double k2 = h*(function2(t + (h/2.0), w + (k1/2.0)) );
+            double k3 = h*(function2(t + (h/2.0), w + (k2/2.0)) ); 
+            double k4 = h*(function2(t + h, w + k3));
+            w = w + (1.0/6.0)*(k1 + 2*k2 + 2*k3 + k4);
+        }
+        else if(que_ecuacion == 3){
+            double k1 = h*(function3(t, w));
+            double k2 = h*(function3(t + (h/2.0), w + (k1/2.0)) );
+            double k3 = h*(function3(t + (h/2.0), w + (k2/2.0)) ); 
+            double k4 = h*(function3(t + h, w + k3));
+            w = w + (1.0/6.0)*(k1 + 2*k2 + 2*k3 + k4);
+        }
+        else if(que_ecuacion == 4){
+            double k1 = h*(function4(t, w));
+            double k2 = h*(function4(t + (h/2.0), w + (k1/2.0)) );
+            double k3 = h*(function4(t + (h/2.0), w + (k2/2.0)) ); 
+            double k4 = h*(function4(t + h, w + k3));
+            w = w + (1.0/6.0)*(k1 + 2*k2 + 2*k3 + k4);
+        }
+
+        fprintf(fptr, "%f\t %f \t numero de thread:%d\n", t+h, w,omp_get_thread_num());
+    }
+
+    fclose(fptr);
+}
+
+
+int main()
+{
+    char nombre1[25] = "Runge1_paral.txt";
+    char nombre2[25] = "Runge2_paral.txt";
+    char nombre3[25] = "Runge3_paral.txt";
+    char nombre4[25] = "Runge4_paral.txt";   
+
+    const double startTime = omp_get_wtime();
+
+    #pragma omp parallel
+    {
+        #pragma omp sections
+        {
+            #pragma omp section
+                (void) rungekuta(mi_N, nombre1, 1);
+            #pragma omp section
+                (void) rungekuta(mi_N, nombre2, 2);
+            #pragma omp section
+                (void) rungekuta(mi_N, nombre3, 3);
+            #pragma omp section
+                (void) rungekuta(mi_N, nombre4, 4);
+        }
+    }
+
+    const double endTime = omp_get_wtime();
+    printf("tomo (%lf) segundos\n", (endTime -startTime));
+
+}
